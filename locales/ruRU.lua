3f8ad3021e76463777bd57a18f1ef1a66a61329a
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2026-02-05 10:18:32 +0300

    Create ruRU.lua

---
+++ locales/ruRU.lua
@@ -0,0 +1,11 @@
+-- Translator ZamestoTV
+local L = {
+L["ADDON_NAME"]        = "MidnightTooltip"
+L["TOOLTIP_HEADER"]    = "Подсказка Midnight"
+L["TOOLTIP_DESCRIPTION"] = "Расширяет всплывающие подсказки дополнительной информацией."
+L["CONFIG_TITLE"]      = "Настройки MidnightTooltip"
+L["CONFIG_ENABLE"]     = "Включить аддон"
+L["CONFIG_DISABLE"]    = "Отключить аддон"
+L["CONFIG_TOOLTIP_OPTIONS"] = "Параметры подсказок"
+}
+MidnightTooltip_Locale = L
